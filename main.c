deb2c26168362f5726fe7fb7ba1cc1b7c412207c
Rodrigo Macedo <104858896+RodrigoMacedo99@users.noreply.github.com>
2024-03-23 05:02:22 -0300

    delete biblioteca

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "control.h"
-#include "model.h"
-#include "pilha.h"
 
+int main() {
+    tp_pilha pecas;
+    tp_fila jogadores;
 
-int main(){
-    configuracao_inicial();
+    // Inicializando o jogo criando as peças de dominó, embaralhando-as e inicializando a mesa e adicionando os jogadores
+    configuracao_inicial(&pecas, &jogadores);
+
     return 0;
 }
