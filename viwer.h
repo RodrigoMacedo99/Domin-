8be78bcd85be7e17fa5379a70a4232ce277e08af
Rodrigo Macedo <104858896+RodrigoMacedo99@users.noreply.github.com>
2024-04-16 23:37:06 -0300

    Update viwer - função
    Alterar função introdução;
    Alterar função inicializar_jogadores.

--- viwer.h
+++ viwer.h
@@ -3,43 +3,47 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 #include "pilha.h"
 #include "fila.h"
+#include "lista_de.h"
 #include "model.h"
 
 
 int introducao(){
 
     int opcao;
-
-    printf("\n\n\<=======>BEM VINDO AO JOGO DE DOMINO <=======>\n\n");
-    printf("Selecione a quantidade de jogadores: [2] [4]\n");
-    printf("> ");
-
-    scanf("%d", &opcao);
+    while(1){
+        printf("\n<=======>BEM VINDO AO JOGO DE DOMINO <=======>\n\n");
+        printf("Selecione a quantidade de jogadores: [2] [4]\n");
+        printf("> ");
+        scanf("%d", &opcao);
+        if(opcao == 2 || opcao == 4){
+            break;
+        }else{  
+            printf("\n\nOpcao invalida, tente novamente!\n\n");
+            Sleep(1000);
+            system("cls");
+        }
+    }
+	system("cls");
+	
     return opcao;
 }
-  #define MAX_JOGADORES 4
 
 void inicializar_jogadores (tp_fila *fila_jogadores, int quantidade_jogadores){
 
-    jogador jogadores;  
-    if ( quantidade_jogadores>MAX_JOGADORES||quantidade_jogadores<2){
-        printf("Qantidade de Jogadores Invalida\n");
-        //exit(1);
-        return;
-    }else{
-    item_jogador jogadores;
+    jogador jogadores;
     for(int i = 1; i <= quantidade_jogadores; i++){        
         //Atribuindo o id do jogador
         jogadores.id_do_jogador = i;
 
         //Recebendo o nome do jogador
+        fila_jogadores->item[i].mao = inicializa_listad();
         printf("\n Digite o nome do jogador %d: ", jogadores.id_do_jogador);
         scanf("%s", jogadores.nome);
         insere_fila(fila_jogadores, jogadores);
         fflush(stdin);
-    }
     }
     printf("\n\nJogadores cadastrados com sucesso!\n\n");
     
