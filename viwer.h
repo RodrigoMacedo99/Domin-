2b4ab493ec4c6d37df3953dd37788d9005b26603
Lans-Anls <andre3@aln.senaicimatec.edu.br>
2024-04-16 23:30:30 -0300

    Limite na funçaõ introdução
    limite da função intro

--- viwer.h
+++ viwer.h
@@ -3,36 +3,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include "pilha.h"
 #include "fila.h"
-#include "lista_de.h"
 #include "model.h"
 
 
 int introducao(){
 
     int opcao;
-    while(1){
-        printf("\n<=======>BEM VINDO AO JOGO DE DOMINO <=======>\n\n");
-        printf("Selecione a quantidade de jogadores: [2] [4]\n");
-        printf("> ");
-        scanf("%d", &opcao);
-        if(opcao == 2 || opcao == 4){
-            break;
-        }else{  
-            printf("\n\nOpcao invalida, tente novamente!\n\n");
-            Sleep(1000);
-            system("cls");
-        }
-    }
-	system("cls");
-	
+
+    printf("\n\n\<=======>BEM VINDO AO JOGO DE DOMINO <=======>\n\n");
+    printf("Selecione a quantidade de jogadores: [2] [4]\n");
+    printf("> ");
+
+    scanf("%d", &opcao);
     return opcao;
 }
+  #define MAX_JOGADORES 4
 
 void inicializar_jogadores (tp_fila *fila_jogadores, int quantidade_jogadores){
 
+    jogador jogadores;  
+    if ( quantidade_jogadores>MAX_JOGADORES||quantidade_jogadores<2){
+        printf("Qantidade de Jogadores Invalida\n");
+        //exit(1);
+        return;
+    }else{
     item_jogador jogadores;
     for(int i = 1; i <= quantidade_jogadores; i++){        
         //Atribuindo o id do jogador
@@ -43,6 +39,7 @@ void inicializar_jogadores (tp_fila *fila_jogadores, int quantidade_jogadores){
         scanf("%s", jogadores.nome);
         insere_fila(fila_jogadores, jogadores);
         fflush(stdin);
+    }
     }
     printf("\n\nJogadores cadastrados com sucesso!\n\n");
     
